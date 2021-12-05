863e9b865544517bcb6704b35fad98b603361856
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-05 13:49:24 +0900

    Initial commit

---
+++ bin/data_processing.dart
@@ -0,0 +1,5 @@
+import 'package:data_processing/data_processing.dart' as data_processing;
+
+void main(List<String> arguments) {
+  print('Hello world: ${data_processing.calculate()}!');
+}
---
+++ test/data_processing_test.dart
@@ -0,0 +1,8 @@
+import 'package:data_processing/data_processing.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
