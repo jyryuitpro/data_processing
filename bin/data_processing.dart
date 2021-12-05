aac1c6558feb307bda9f303bad7cce539e076166
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-05 14:40:08 +0900

    Data Processing

--- bin/data_processing.dart
+++ bin/data_processing.dart
@@ -1,5 +1,34 @@
+import 'dart:io';
+
 import 'package:data_processing/data_processing.dart' as data_processing;
 
 void main(List<String> arguments) {
-  print('Hello world: ${data_processing.calculate()}!');
+  if (arguments.isEmpty) {
+    print('Usage: dart data_processing.dart <inputFile.csv>');
+    exit(1);
+  }
+  final inputFile = arguments.first;
+  final lines = File('./lib/'+inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  lines.removeAt(0);
+  var totalDuration = 0.0;
+  for (var line in lines) {
+    final values = line.split(',');
+    final durationString = values[3].replaceAll('"', '');
+    final duraton = double.parse(durationString);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duraton;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duraton;
+    }
+    totalDuration += duraton;
+  }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    final tag = entry.key == '' ? 'Unallocated' : entry.key;
+    print('$tag: ${durationFormatted}h');
+  }
+  print('Total for all tags: ${totalDuration.toStringAsFixed(1)}h');
 }
